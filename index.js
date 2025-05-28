8e2577df2ad52b0a14e06a5a29cc68002a8874bd
tubarao10 <miguelservosdias1005@gmail.com>
2025-05-27 21:29:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+ function mostrarMensagem() {
+            // Acessa o elemento com o id "mensagem" e altera seu estilo para exibi-lo
+            document.getElementById('mensagem').style.display = 'block';
+        }
